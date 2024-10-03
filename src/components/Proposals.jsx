6fdc8f27300826c3e6ee31df62398a0f589b78db
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-10-03 15:09:28 +0100

    change

--- src/components/Proposals.jsx
+++ src/components/Proposals.jsx
@@ -1,14 +1,15 @@
-import { Box, Text } from "@radix-ui/themes";
+import { Box, Flex, Text } from "@radix-ui/themes";
+import React from "react";
 import Proposal from "./Proposal";
 
 const Proposals = ({ proposals }) => {
   return (
     <Box className="grid w-full grid-cols-3 gap-2 p-8">
-      {proposals && proposals.length === 0 ? (
+      {proposals.length === 0 ? (
         <Text>No data to display</Text>
       ) : (
-        proposals.map((proposal, i) => (
-          <Proposal key={proposal?.proposalId || i} proposal={proposal} />
+        proposals.map((proposal) => (
+          <Proposal key={proposal.proposalId} proposal={proposal} />
         ))
       )}
     </Box>
